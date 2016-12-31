c57209bd8f5ca749102470a2a564cfc0e09cb5a2
valtena <ethanvaltena@gmail.com>
2016-12-31 01:14:26 +0100

    fonctionnel en le lançant avec:
    -d .\target\classes\challenges\ "challenges.FirstChallenge challenges.TestChallenge"

--- src/dd/impl/ChallengesLoader.java
+++ src/dd/impl/ChallengesLoader.java
@@ -1,28 +1,28 @@
-package dd.impl;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-public class ChallengesLoader extends ClassLoader {
-	
-	public ChallengesLoader(ClassLoader parent) {
-        super(parent);
-    }
-	
-	public Class loadClass(Path name) throws ClassNotFoundException {
-        try {
-            byte[] classData = Files.readAllBytes(name);
-
-            return defineClass(name.getFileName().toString(),
-                    classData, 0, classData.length);
-
-        }  catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-	
-
-}
+package dd.impl; 
+ 
+import java.io.IOException; 
+import java.nio.file.Files; 
+import java.nio.file.Path; 
+ 
+public class ChallengesLoader extends ClassLoader { 
+   
+  public ChallengesLoader(ClassLoader parent) { 
+        super(parent); 
+    } 
+   
+  public Class loadClass(Path name) throws ClassNotFoundException { 
+        try { 
+            byte[] classData = Files.readAllBytes(name); 
+ 
+            return defineClass(name.getFileName().toString(), 
+                    classData, 0, classData.length); 
+ 
+        }  catch (IOException e) { 
+            e.printStackTrace(); 
+        } 
+ 
+        return null; 
+    } 
+   
+ 
+} 
--- src/dd/impl/Main.java
+++ src/dd/impl/Main.java
@@ -1,6 +1,7 @@
 package dd.impl;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.net.MalformedURLException;
@@ -21,12 +22,23 @@ public class Main {
 
 	    String curDir = System.getProperty("user.dir");
 	    System.out.println ("Le répertoire courant est: "+curDir);
-		System.out.println(args[0]);
-		System.out.println(args[1]);
 		if(args[0].equals("-f")){
 			runOnFile( Paths.get(args[1]) );
 		}else if(args[0].equals("-d")){
-			runOnDir(args[1]);
+			runOnDir(args[1], args[2].split(" "));
+		}
+	}
+	
+	public static void executeOnClass(Class cls){
+		try {
+			Object aChallenge = cls.newInstance();
+			if(aChallenge instanceof Challenge){
+				CauseEffectChain cec = d.debug((Challenge) aChallenge);
+				System.out.println(cec);
+			}
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
@@ -35,20 +47,14 @@ public class Main {
 		Class aClass;
 		try {//"D:\\git\\CauseEffectChains\\src\\classes\\challenges\\FirstChallenge.java"
 			aClass = classLoader.loadClass(path);
-		
-			Object aChallenge= aClass.newInstance();
-			System.out.println(path);
-			if(aChallenge instanceof Challenge){
-				CauseEffectChain cec = d.debug((Challenge) aChallenge);
-				System.out.println(cec);
-			}
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			executeOnClass(aClass);
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static void runOnDir(String path){
+	public static void runOnDir(String path, String[] classes){
 		// Create a File object on the root of the directory containing the class file
 		File file = new File(path);
 
@@ -59,22 +65,23 @@ public class Main {
 
 		    // Create a new class loader with the directory
 		    ClassLoader cl = new URLClassLoader(urls);
-		    
-		    Class cls = cl.loadClass("challenges.FirstChallenge");
-		    Object aChallenge;
-			try {
-				aChallenge = cls.newInstance();
-				if(aChallenge instanceof Challenge){
-					CauseEffectChain cec = d.debug((Challenge) aChallenge);
-					System.out.println(cec);
-				}
-			} catch (InstantiationException | IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		    int numberOfClass=Array.getLength(classes);
+		    for(int i=0; i< numberOfClass;i++){
+		    	declareNewChallenge(classes[i]);
+			    Class cls = cl.loadClass(classes[i]);
+		    	executeOnClass(cls);
+		    }
 		    
 		} catch (MalformedURLException e) {
 		} catch (ClassNotFoundException e) {
 		}
+	}
+	
+	public static void declareNewChallenge(String cls){
+		System.out.println("");
+		System.out.println("------------------------------------------------");
+		System.out.format("|| Challenge :  %s   ||\n",cls);
+		System.out.println("------------------------------------------------");
+		System.out.println("");
 	}
 }
