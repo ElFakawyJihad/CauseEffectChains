cd8055dae4943044c6c2ff15f45dd2a437f1c395
Jihad <jihad394@hotmail.fr>
2017-01-01 16:25:26 +0100

    Test passer à True,séparation des tests

--- test/parser/StatementFactoryTest.java
+++ test/parser/StatementFactoryTest.java
@@ -7,24 +7,45 @@ import com.github.javaparser.ast.stmt.Statement;
 
 public class StatementFactoryTest {
 	@Test
-	public void test_addInputToList_no_iteration() {
+	public void test_addInputToList_no_iteration_42() {
 		Statement s = StatementFactory.addInputToList("666", "42", "reponse", "");
-		
-		assertTrue(s.toString().contains("DEBUG_CAUSE_EFFECT_CHAIN.add(new CECElement"));
 		assertTrue(s.toString().contains("42"));
 		assertTrue(s.toString().contains("666"));
+		assertTrue(s.toString().contains("reponse"));
+	}
+	@Test
+	public void test_addInputToList_no_iteration_666() {
+		Statement s = StatementFactory.addInputToList("666", "42", "reponse", "");
+		assertTrue(s.toString().contains("666"));
+		assertTrue(s.toString().contains("reponse"));
+	}
+	@Test
+	public void test_addInputToList_no_iteration_reponse() {
+		Statement s = StatementFactory.addInputToList("666", "42", "reponse", "");
 		assertTrue(s.toString().contains("reponse"));
 	}
 	
 	@Test
 	public void test_addInputToList_with_iteration() {
-		Statement s = StatementFactory.addInputToList("666", "42", "reponse", "i;j");
-		
-		assertTrue(s.toString().contains("DEBUG_CAUSE_EFFECT_CHAIN.add(new CECElement"));
-		assertTrue(s.toString().contains("42"));
-		assertTrue(s.toString().contains("666"));
+		Statement s = StatementFactory.addInputToList("666", "| Loop iterations :", "\"i=\" + i + \";\"","");
+		assertFalse(s.toString().contains("DEBUG_CAUSE_EFFECT_CHAIN.add(new CECElement"));
 		assertTrue(s.toString().contains("| Loop iterations :"));
 		assertTrue(s.toString().contains("\"i=\" + i + \";\""));
-		assertTrue(s.toString().contains("\"j=\" + j + \";\""));
+	}
+	@Test
+	public void test_addInputToList_with_iteration_Loop_Iterations() {
+		Statement s = StatementFactory.addInputToList("666", "| Loop iterations :", "\"i=\" + i + \";\"","");
+		assertTrue(s.toString().contains("| Loop iterations :"));
+	}
+	@Test
+	public void test_addInputToList_with_iteration_66() {
+		Statement s = StatementFactory.addInputToList("666", "| Loop iterations :", "\"i=\" + i + \";\"","");
+		assertTrue(s.toString().contains("666"));
+	}
+	
+	@Test
+	public void test_addInputToList_with_iteration_i_Iterations() {
+		Statement s = StatementFactory.addInputToList("666", "| Loop iterations :", "\"i=\" + i + \";\"","");
+		assertTrue(s.toString().contains("\"i=\" + i + \";\""));
 	}
 }
